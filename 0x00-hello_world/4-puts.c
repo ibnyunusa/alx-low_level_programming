c7f6a22ae4547584007e1a2af2a843e8e7cf3605
Yunusa Muhammed <ibnyunusa13@gmail.com>
2023-05-18 20:45:43 -0700

    My first low level program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#include <stdio>
+#include <stdio.h>
 
-/*
- *Main - Entry point
- *
- *Return: Always 0(sucess)
- *
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
-int main(void)
 
+int main(void)
 {
-  
-  puts("\Programming is like building a multilingual puzzle\n");
-  return(0);
-  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

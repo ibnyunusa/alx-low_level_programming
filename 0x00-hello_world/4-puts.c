97eb8e19fa8a4cfdf8006bbc96d7eda20cde8cce
Yunusa Muhammed <ibnyunusa13@gmail.com>
2023-05-18 20:05:28 -0700

    My first low level program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/*
+ *Main - Entry point
+ *
+ *Return: Always 0(sucess)
+ *
+ */
+int main(void)
+
+{
+  puts("\Programming is like building a multilingual puzzle\n");
+  return(0);
+}

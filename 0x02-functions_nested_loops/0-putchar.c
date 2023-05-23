c95099d72f04850de31e2b1a411cc1538532bf34
ibnyunus <ibnyunusa13@gmail.com>
2023-05-23 09:37:29 -0700

    C function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - calling a function the print _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x020funtions nested_loops

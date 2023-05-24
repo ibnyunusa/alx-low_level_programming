7dcae788b23221e68b9fb6e87e28176d6acb9962
ibnyunus <ibnyunusa13@gmail.com>
2023-05-24 02:22:21 -0700

    C function loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check for the lowercase alphabet
+ * _islower - check for the lowercase alphabet
+ * @c: is the character to be cheched
  * Return: 1(sucess)
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	if (r >= 97 && r <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
